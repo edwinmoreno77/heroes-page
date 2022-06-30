cc69b5219e125ef5d0527addd4941e320fd227d5
Edwin Moreno <edwinmoreno77@gmail.com>
2022-06-29 22:25:50 -0400

    assets changed

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -10,7 +10,7 @@ export const HeroCard = ({
     characters,
 }) => {
 
-    const heroImageUrl = `/assets/heroes/${id}.jpg`;
+    const heroImageUrl = `/img/heroes/${id}.jpg`;
 
     return (
         <div className='col animate__animated animate__fadeIn'>
